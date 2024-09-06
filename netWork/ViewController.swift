d213a84d5f876d2c4ec009cbb13d03036c8c7e69
ravindar <ravindar.banoth@myn.team>
2024-09-06 14:46:48 +0530

    Initial Commit

---
+++ netWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netWork
+//
+//  Created by Ravindar Nayak on 06/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
